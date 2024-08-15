efb1cfccbc9fa62aaeb95419dae241016a2f1272
chihebgmc <chihebchabougmc@gmail.com>
2024-08-15 14:51:30 +0100

    update variable script.js

--- 01_variables/script.js
+++ 01_variables/script.js
@@ -35,3 +35,57 @@ console.log('type of y: ' + typeof y);
 var z = null;
 console.log(z);
 console.log('type of z: ' + typeof z);
+
+// 1. Arithmetic Operators
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+// Concatenation
+x = 'Hello' + ' ' + 'World';
+
+// Exponent
+x = 2 ** 3;
+
+// Increment
+x = 1;
+// x = x + 1;
+x++;
+
+// Decrement
+// x = x - 1;
+x--;
+
+// 2. Assignment Operators
+x = 10;
+
+x += 5; // same as x = x + 5
+x -= 5; // same as x = x - 5
+x *= 5; // same as x = x * 5
+x /= 5; // same as x = x / 5
+x %= 5; // same as x = x % 5
+x **= 5; // same as x = x ** 5
+
+// 3. Comparison Operators
+
+// Equal to (Just the value, not the type)
+x = 2 == '2';
+
+// Equal to (Type and value)
+x = 2 === '2';
+
+// Not equal to (Just the value, not the type)
+x = 2 != '2';
+
+// Not equal to (Type and value)
+x = 2 !== 2;
+
+// Greater than and less than
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
